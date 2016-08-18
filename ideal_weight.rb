130c858bbedf35cf0636dacf5375e60ecba88375
Kristinali <krestia18@gmail.com>
2016-08-18 21:11:21 +0300

    Correction

--- ideal_weight.rb
+++ ideal_weight.rb
@@ -1,16 +1,16 @@
 puts "Здравствуйте! Как вас зовут?"
 name = gets.chomp.capitalize
 
-
 puts "Напишите, пожалуйста, свой рост в сантиметрах."
-height = gets.chomp.to_i
+height = gets.chomp.to_f
+
 ideal_weight = height - 110
 
 # либо можно не вводить ideal_weight, а выражать через height - 110, но в данном виде код мне кажется более понятным
 
 if ideal_weight >= 0
-	puts "#{name}, ваш идеальный вес #{ideal_weight}!"
-	else
-	puts "#{name}, ваш вес уже оптимальный."	
+  puts "#{name}, ваш идеальный вес #{ideal_weight} кг!"
+else
+  puts "#{name}, ваш вес уже оптимальный."    
 end
 
--- right_triangle.rb
+++ right_triangle.rb
@@ -1,38 +1,31 @@
 puts "Привет! Помогу тебе определить, является ли треугольник прямоугольным. Для начала введи длину первой стороны треугольника."
-a = gets.chomp.to_i
+a = gets.chomp.to_f
 
 puts "Теперь напиши, какая длина у второй стороны."
-b = gets.chomp.to_i
+b = gets.chomp.to_f
 
 puts "Наконец, какой длины третья сторона?"
-c = gets.chomp.to_i
+c = gets.chomp.to_f
 
 lenght = [a, b, c]
-
 lenght.sort!
 
 cathetus1 = lenght[0]
 cathetus2 = lenght[1]
 hypotenuse = lenght[2]
 
-if cathetus1 * cathetus1 + cathetus2 * cathetus2 == hypotenuse * hypotenuse
- puts "Треугольник с такими параметрами является прямоугольным."
-
- else 
-	puts "Это не прямоугольный треугольник!"
-
+if cathetus1 ** 2 + cathetus2 ** 2 == hypotenuse ** 2
+  puts "Треугольник с такими параметрами является прямоугольным."
+else 
+  puts "Это не прямоугольный треугольник!"
 end
 
 if cathetus1 == cathetus2 && cathetus2 != hypotenuse
-	puts "Треугольник с такими параметрами является равнобедренным."
-
+  puts "Треугольник с такими параметрами является равнобедренным."
 elsif cathetus2 == hypotenuse && cathetus1 != hypotenuse
-		puts "Треугольник с такими параметрами является равнобедренным."
-
+  puts "Треугольник с такими параметрами является равнобедренным."
 elsif cathetus1 == hypotenuse && cathetus1 != cathetus2
-		puts "Треугольник с такими параметрами является равнобедренным."
-
+  puts "Треугольник с такими параметрами является равнобедренным."
 elsif cathetus1 == cathetus2 && cathetus2 == hypotenuse
-	puts "Треугольник с такими параметрами является равнобедренным и равносторонним."
-	
+  puts "Треугольник с такими параметрами является равнобедренным и равносторонним." 
 end
